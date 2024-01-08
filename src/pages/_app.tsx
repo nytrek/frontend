81aa7a29fb499d55d91a9d416763644ce9a45088
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2024-01-08 09:28:26 +0100

    add hotjar tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -68,6 +68,17 @@ export default function App({ Component, pageProps, router }: AppProps) {
           font-family: ${albert_sans.style.fontFamily};
         }
       `}</style>
+      {/* <!-- Hotjar Tracking Code for https://www.bostadsval.se/ --> */}
+      <Script id="hotjar" strategy="afterInteractive">
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3816802,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-NEM0C7BL54"
         strategy="afterInteractive"
